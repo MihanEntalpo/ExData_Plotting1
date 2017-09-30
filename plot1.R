826bd967b68730348d79f125adabed2dce520c83
MihanEntalpo <mihanentalpo@yandex.ru>
2017-09-30 20:10:48 +0700

    Removed data-file, added algorythm for auto download of the file
    Former-commit-id: 85e0be6cdc23c2afe420702ff6b6e80421592fcc

--- plot1.R
+++ plot1.R
@@ -13,8 +13,18 @@ setwd(scriptPath())
 load_data = function() {
     filename = paste(getwd(), "household_power_consumption.txt", sep="/")
     print("Reading data...")
+    if (!file.exists(filename))
+    {
+        print("Downloading data file...")
+        zipfile = paste(filename, ".zip", sep="")
+        url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(url, destfile=zipfile, method="curl")
+        print("Unpacking file...")
+        unzip(zipfile)
+    }
     data = read.table(filename, header=TRUE, sep=";", na.strings = "?")
     data$Date = as.Date(data$Date, "%d/%m/%Y")
+    data$DateTime = as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
     print("Filtering by date...")
     dates = as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
     data = subset(data, Date %in% dates)
--- plot2.R
+++ plot2.R
@@ -14,6 +14,15 @@ setwd(scriptPath())
 load_data = function() {
     filename = paste(getwd(), "household_power_consumption.txt", sep="/")
     print("Reading data...")
+    if (!file.exists(filename))
+    {
+        print("Downloading data file...")
+        zipfile = paste(filename, ".zip", sep="")
+        url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(url, destfile=zipfile, method="curl")
+        print("Unpacking file...")
+        unzip(zipfile)
+    }
     data = read.table(filename, header=TRUE, sep=";", na.strings = "?")
     data$Date = as.Date(data$Date, "%d/%m/%Y")
     data$DateTime = as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
